13d8752add8cde78dd27d3bcc677d4b04ec04df9
afroprogrammer <forsonfranklin@gmail.com>
2020-06-07 00:01:16 +0000

    Catch Off By One Errors When Using Indexing  challenge complted

---
+++ catch-off-by-one-errors-when-using-indexing.js
@@ -0,0 +1,30 @@
+/**
+ let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let len = alphabet.length;
+for (let i = 0; i <= len; i++) {
+  // loops one too many times at the end
+  console.log(alphabet[i]);
+}
+for (let j = 1; j < len; j++) {
+  // loops one too few times and misses the first character at index 0
+  console.log(alphabet[j]);
+}
+for (let k = 0; k < len; k++) {
+  // Goldilocks approves - this is just right
+  console.log(alphabet[k]);
+}
+Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console.
+ */
+
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  // Only change code below this line
+  for (let i = 0; i < len; i++) {
+  // Only change code above this line
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
+
